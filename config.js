9d1d205745039de383e8dc73c4f2de8a25a14ba9
BarreraE20 <119444310+BarreraE20@users.noreply.github.com>
2022-12-03 21:16:25 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyNOm-9qE7x2f8ArReO0kzbre-IIWJwfwMjcg7E6WgkAxmfa_Cv_-UBMc0aC0VVH5F4BA/exec";
